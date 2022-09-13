b2a23bbb8b0340a204e18a520593edb4b11906b6
Ivaylo Petrov <i.petrow@live.com>
2022-09-13 08:26:13 +0300

    Added JS, working calculator

--- script.js
+++ script.js
@@ -1,22 +1,122 @@
-const result = document.getElementById("calc-output")
-const calculationField = document.getElementById("calculation")
-const numberButtons = document.querySelectorAll(".numbers")
-let currNumber = ""
+const calcResult = document.getElementById("calc-output")
+const calcField = document.getElementById("calculation")
+const numButtons = document.querySelectorAll(".numbers")
+const operatorButtons = document.querySelectorAll(".operator")
+const clearAllButton = document.querySelector(".clear-all")
+const clearOneButton = document.querySelector(".clear-entry")
+const equalsButton = document.querySelector(".equals")
+const dotButton = document.querySelector(".dot")
+let currNum = ""
+let nextNum = ""
+let operator = ""
+let result = ""
 
+clearAllButton.addEventListener("click", (e) => clearAll())
+equalsButton.addEventListener("click", (e) => equals())
+clearOneButton.addEventListener("click", (e) => clearLast())
 
-// Run all functions 
-function main() {
-    selectNumber()
-}
+// Adding float numbers
+dotButton.addEventListener("click", (e) => {
+    if (operator == "" && !currNum.includes(".")) {
+        if (currNum == "") {
+            currNum += "0."
+        } else {
+            currNum += "."
+        }
+    } else if (operator != "" && !nextNum.includes(".")) {
+        if (nextNum == "") {
+            nextNum += "0."
+        } else {
+            nextNum += "."
+        }
+    }
+    updateDisplay()
+})
 
-main()
-
-// Create a number
-function selectNumber() {
-    numberButtons.forEach(button => {
-        button.addEventListener("click", (e) => {
-            currNumber += button.value
-            calculationField.innerHTML = currNumber
-          })
+// Select numbers
+numButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        equalsButton.removeAttribute('disabled', '')
+        if (operator == "") {
+            currNum == "0" ? currNum = button.textContent : currNum += button.textContent
+        } else {
+            nextNum == "0" ? nextNum = button.textContent : nextNum += button.textContent
+        }
+        updateDisplay()
     })
+})
+
+// Select operator
+operatorButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        equalsButton.removeAttribute('disabled', '')
+        if (currNum != "" && nextNum != "") {
+            equals()
+        }
+        operator = button.textContent
+        updateDisplay()
+    })
+})
+
+// Update calculation display
+function updateDisplay() {
+    calcField.textContent = currNum + " " + operator + " " + nextNum
+    if (result != "") calcResult.textContent = result
 }
+
+// Clear all fields 
+function clearAll() {
+    currNum = ""
+    nextNum = ""
+    operator = ""
+    nextOperator = ""
+    result = ""
+    calcField.textContent = ""
+    calcResult.textContent = "0"
+}
+
+// Clear last entry
+function clearLast() {
+    operator == "" ? currNum = currNum.slice(0, -1) : nextNum = nextNum.slice(0, -1)
+    updateDisplay()
+}
+
+// Operations
+function operate(operator, a, b) {
+    switch(operator) {
+        case "+":
+            return parseFloat(a) + parseFloat(b)
+            break
+        case "-":
+            return parseFloat(a) - parseFloat(b)
+            break
+        case "*":
+            return parseFloat(a) * parseFloat(b)
+            break
+        case "/":
+            if (a == 0 || b == 0) return "dividing by 0.."
+            return parseFloat(a) / parseFloat(b)
+            return 
+            break
+        case "%":
+            return parseFloat(a) % parseFloat(b)
+            break
+    }
+}
+
+// Do the equation and print result
+function equals() {
+    if (currNum != "" && nextNum != "" && operator != "") {
+        result = operate(operator, currNum, nextNum)
+        result = !isInt(result) ? result.toFixed(2) : parseInt(result)
+        equalsButton.setAttribute('disabled', '')
+        updateDisplay()
+        currNum = result
+        nextNum = ""
+    }
+}
+
+// Check is number is int/float
+function isInt(n) {
+    return n % 1 === 0;
+ }
